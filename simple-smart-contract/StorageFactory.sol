de2ae3c317738713106fcf87cf59a2a9b303edf4
Ole Sorensen <raproid@gmail.com>
2024-06-22 02:29:34 +0300

    add AddFiveStorage.sol, update StorageFactory.sol with array-related func

--- simple-smart-contract/StorageFactory.sol
+++ simple-smart-contract/StorageFactory.sol
@@ -6,12 +6,32 @@ import "./SimpleStorage.sol";
 
 contract StorageFactory{
 
-    // declare a var to hold new SimpleStorage objects
+    // declare a var to hold the addr of a new SimpleStorage instance
     SimpleStorage public simpleStorage;
 
-    // create new SimpleStorage objects
+    // declare a dynamic array of SimpleStorage contracts
+    SimpleStorage[] public  listOfSimpleStorageContracts;
+
+    // create new SimpleStorage instance
     function createSimpleStorage() public {
         simpleStorage = new SimpleStorage();
     }
+
+    // push SimpleStorage contracts into SimpleStorage[]
+    function createSimpleStorageAndAddToArray() public {
+        SimpleStorage newSimpleStorageContract = new SimpleStorage();
+        listOfSimpleStorageContracts.push(newSimpleStorageContract);
+    }
+
+    // store a number in the array
+    function sfStore(uint256 _simpleStorageIndex, uint256 _newSimpleStorageNumber) public {
+        listOfSimpleStorageContracts[_simpleStorageIndex].store(_newSimpleStorageNumber);
+    }
+
+    // get a number by the index from the array
+    function sfGet(uint256 _simpleStorageIndex)public view returns(uint256) {
+        return listOfSimpleStorageContracts[_simpleStorageIndex].retrieve();
+    }
+
 }
 
