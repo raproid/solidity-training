3a85a2e88253667d10a611eadd3ef632cb1be844
Ole Sorensen <raproid@gmail.com>
2025-03-12 22:49:21 +0300

    redirect sender who send ETH without calling fund() to fund()

--- simple-funding-smart-contract/FundMe.sol
+++ simple-funding-smart-contract/FundMe.sol
@@ -27,7 +27,7 @@ contract FundMe {
 
     constructor() {
         i_owner = msg.sender;
-}
+    }
 
     function fund() public payable {
         // Allow users to send money
@@ -41,20 +41,16 @@ contract FundMe {
 
     function withdraw() public onlyOwner {
     // Withdraw accumulated funds to the sc owner's wallet
-
         // Reset all the mapping to 0
         for(uint256  funderIndex = 0; funderIndex < funders.length; funderIndex++) {
             address funder = funders[funderIndex];
             addressToAmountFunded[funder] = 0;
+        }
         // Reset the array
         funders = new address[](0);
-        }
-
         // payable(msg.sender).transfer(address(this).balance);
-
         // bool sendSuccess = payable(msg.sender).send(addresss(this).balance);
         // require(sendSuccess, "Send failed");
-
         // Withdraw funds
         (bool callSuccess, ) = payable(msg.sender).call{value: address(this).balance}("");
         require(callSuccess, "Call failed");
@@ -63,15 +59,13 @@ contract FundMe {
     modifier onlyOwner() {
        // require() eats up too much gas; optimizing gas costs with revert(); not sure if implemented it correctly, as it's still passed a string down below and revert doesn't have a func syntax there
        // require(msg.sender == i_owner, "Sender is not owner");
-
        if (msg.sender != i_owner) {
         revert NotOwner("Sender is not owner");
        }
-        _;
+       _;
     }
 
-    // If somebody sends ETH to this contract without calling fund(), redirect them to fund()
-
+    // If somebody sends ETH to this contract/sends data without calling fund() , redirect them to fund()
     receive() external payable {
         fund();
     }
